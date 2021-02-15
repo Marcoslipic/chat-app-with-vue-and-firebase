d09c93e03a194279543e22d1329e76309630fa39
Marcoslipic <mjlipic.37@gmail.com>
2021-02-14 21:00:11 -0700

    send message with firebase

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,89 @@
 <template>
-<h1>hello world!</h1> 
+<div class="view login" v-if="state.username === '' || state.username === null">
+  <form class="login-form" @submit.prevent="Login">
+    <div class="form-inner">
+      <h1>Login to Chat</h1>
+      <label for="username">Username</label>
+      <input type="text" v-model="inputUsername" placeholder="Please enter your username" />
+      <input type="submit" value="Login" />
+    </div>
+  </form>
+</div>
+<div class="view chat" v-else>
+  <header>
+    <button class="logout">Logout</button>
+    <h1>Welcome, {{ state.username }}</h1>
+  </header>
+  <section class="chat-box">
+    // Messages 
+  </section>
+  <footer>
+    <form @submit.prevent="SendMessage">
+      <input type="text" v-model="inputMessage" placeholder="Write a message..." />
+      <input type="submit" value="Send" />
+    </form>
+  </footer>
+</div>
 </template>
 
 <script>
+import { reactive, onMounted, ref } from 'vue'
 import db from './db'
 
 export default {
   setup() {
-    return{} 
+    const inputUsername = ref("");
+    const inputMessage = ref("");
+
+    const state = reactive({
+      username: "",
+      messages: []
+    });
+
+    const Login = () => {
+      if (inputUsername.value != "" || inputUsername.value != null) {
+        state.username = inputUsername.value;
+        inputUsername.value = ""; 
+      }
+    }
+
+    const SendMessage = () => {
+      const messagesRef = db.database().ref("messages"); 
+
+      if (inputMessage.value === "" || inputMessage.value === null) {
+        return;
+      }
+
+      const message = {
+        username: state.username,
+        content: inputMessage.value 
+      }
+
+      messagesRef.push(message);
+      inputMessage.value = "";
+    }
+
+    // onMounted(() => {
+    //   const messagesRef = db.database().ref("messages");
+
+    //   messagesRef.on('value', snapshot => {
+    //     const data = snapshot.val(); 
+    //   })
+    // })
+
+    return{
+      inputUsername,
+      Login,
+      state,
+      inputMessage,
+      SendMessage
+    } 
   }
 }
 </script>
 
 <style lang="scss">
-{
+* {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
